8ccf76b3b997d92ddd56d99e27fb17b6d45c49be
Henrihva <h.vieira.senai@gmail.com>
2024-05-15 17:33:56 -0300

    Implementação do Hello Word
    Implementação do Hello Word em varias linguagens

--- projects/hello.py
+++ projects/hello.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
-print('Hello Word !')
+print('Hello World !')
 print ('Henri'.upper())
 print (60 + 5 - 4)
-print('------------------')
+
+print('---------------------')
+
+# 
---
+++ projects/hello2.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+'''
+Helo word Multi Linguas
+
+dependendo da lingua configurada no computador (ambiente) o programa exibirá
+o Hello Word correspondente 
+
+Como usar:
+Tenha a variavel configurada no seu ambiente exemplo: LANG=pt_BR.UTF-8
+Execução :
+    python3 hello2.py
+    ou 
+    ./hello2.py
+    
+'''
+import os 
+
+# Dunder = palavra em ingles que significa __ (duplo anderline) 
+__version__ = "0.0.1"
+__author__ = "Henri Alves"
+__license__ = "Unlicense"
+
+# Este programa vai imprimir o famoso Hello Word na linguagem definida no ambeinte 
+
+#if __name__ == "__main__":
+#    print('Hello Word !')
+# built-in (pre instalado dentro da linguagem)
+# dois exemplos : variavel[:5]   assim pega os 5 primeiros caracteres 
+# ou 
+# variavel.split(".")  assim divide a variavel em duas 
+"""
+    dentro do python
+    
+    >>> import os
+    >>> variavel = os.getenv("LANG")
+    >>> variavel[:5]
+    'pt_BR'
+    >>> len(variavel)
+    11
+    >>> variavel.split(".")
+    ['pt_BR', 'UTF-8']
+    >>> 
+    >>> 
+"""
+
+
+# snake_case  = tudo minusculo 
+# current_language = os.getenv("LANG")[:5]
+current_language = os.getenv("LANG", "en_US")[:5] 
+# neste caso o segunto parametro é usado quando o objeto é nulo (ou seja quando não existir )
+# a variavel de ambiente LANG setada no ambiente 
+
+msg = 'Hello World !'
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+elif current_language == "ru-RU":
+    msg = "Привет, мир !"
+elif current_language == "fr-FR": # Francês 
+    msg = "Bonjour le monde !"
+elif current_language == "de-DE": # alemão
+    msg = "Hallo Welt !"
+elif current_language == "es-AR":
+    msg = "Hola Mundo !"
+
+
+print(msg)
+
+
+
